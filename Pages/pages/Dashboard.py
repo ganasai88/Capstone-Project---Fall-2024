d6b67ae62b71541dc6a9d48183e562f760cd5555
Mani Surya Teja Kota <145525421+MST-369@users.noreply.github.com>
2024-12-03 16:49:48 -0500

    Update Dashboard.py

--- Pages/pages/Dashboard.py
+++ Pages/pages/Dashboard.py
@@ -243,7 +243,7 @@ if "Student_login" and "st_ID" in st.session_state and st.session_state["Student
                     AT = "NO"
                     confidence = cosine_similarity(np.reshape(pro,(1,-1)),np.reshape(stu,(1,-1)))[0][0]
                     print(confidence)
-                    if confidence>.90:
+                    if confidence>.70:
                         AT = "YES"
                         r = db.table('Attendence').insert([{'Date':dt,'Student ID':ID,'Course ID':int(a_cid),'Attendence': AT,"Time_Start":at_start.strftime('%H:%M:%S.%f'),"Time_End":at_end.strftime('%H:%M:%S.%f')}]).execute()                    
                     if AT=='YES':
