439221524445d44381022601ee9874ddefb8a081
Mani Surya Teja Kota <145525421+MST-369@users.noreply.github.com>
2024-12-06 01:37:46 -0500

    Update Dashboard.py

--- Pages/pages/Dashboard.py
+++ Pages/pages/Dashboard.py
@@ -477,7 +477,7 @@ elif "Admin_login" and "ad_usnm" in st.session_state and st.session_state["Admin
     mc1 = ad11.selectbox('choose',['Read','Unread'])
     if ad11.button('Fetch!'):
         #mc = mc[mc.index('[')+1:mc.index(']')]
-        print(mc,mc.index,list(res['Name'])+IDs)
+        print(mc,list(res['Name'])+IDs)
         mc = int(mc)
         st.session_state['fetch']=True
     if 'fetch' in st.session_state and st.session_state['fetch']:
